ff018b0bd449e0a3d0607be5aeed56ad37ee9d31
Prof. Hugo - SENAC <hugo.tannus@go.senac.br>
2024-09-16 15:38:39 -0300

    Adiciona condições para construir blocos vazios dinamicamente.

--- src/script.js
+++ src/script.js
@@ -1,12 +1,41 @@
-const tabuleiro = document.getElementById('board2');
+buildGameBoard(8, 8);
 
-const linha = document.createElement('div');
-linha.classList.add('row');
+function buildGameBoard(numberOfRows, numberOfCollumns) {
+    const board = document.getElementById('board2');
 
-for(let i=0; i<4; i++) {
-    const celula = document.createElement('div');
-    celula.classList.add('cell');
-    linha.append(celula);
+    for(let k=0; k < numberOfRows; k++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        board.append(row);
+        
+        for(let i=0; i < numberOfCollumns; i++)  {
+            const celula = document.createElement('div');
+            celula.classList.add('cell');
+            row.append(celula);
+
+            if(k == 0 || k == numberOfRows-1 || i == 0 || i == numberOfCollumns-1) {
+                celula.classList.add('empty');
+            }
+
+            /*
+            if(k > 0 && k < numberOfRows-1 && i > 0 && i < numberOfCollumns-1) {
+                celula.classList.add('empty');
+            } */
+
+            /*
+            if(k <= i) {
+                celula.classList.add('empty');
+            } */
+
+            /*
+            if(k + i < numberOfCollumns-1) {
+                celula.classList.add('empty');
+            } */
+
+            /*
+            if(k + i < numberOfRows-1) {
+                celula.classList.add('empty');
+            } */
+        }
+    }
 }
-
-tabuleiro.append(linha);
