4a6ff99b43ee052a8952b8ee154ba6d6a9b9d697
Prof. Hugo - SENAC <hugo.tannus@go.senac.br>
2024-09-16 17:53:43 -0300

    Adiciona função _callback_ para determinar a regra de preenchimento do tabuleiro.

--- src/script.js
+++ src/script.js
@@ -1,41 +1,46 @@
-buildGameBoard(8, 8);
+buildGameBoard('board1', 8, 8, rule1);
+buildGameBoard('board2', 8, 8, rule2);
+buildGameBoard('board3', 8, 8, rule3);
+buildGameBoard('board4', 8, 8, rule4);
 
-function buildGameBoard(numberOfRows, numberOfCollumns) {
-    const board = document.getElementById('board2');
+function buildGameBoard(boardId, numberOfRows, numberOfCollumns, drawRule) {
+    const board = document.getElementById(boardId);
 
-    for(let k=0; k < numberOfRows; k++) {
+    for (let k = 0; k < numberOfRows; k++) {
         const row = document.createElement('div');
         row.classList.add('row');
         board.append(row);
-        
-        for(let i=0; i < numberOfCollumns; i++)  {
+
+        for (let i = 0; i < numberOfCollumns; i++) {
             const celula = document.createElement('div');
             celula.classList.add('cell');
             row.append(celula);
 
-            if(k == 0 || k == numberOfRows-1 || i == 0 || i == numberOfCollumns-1) {
-                celula.classList.add('empty');
-            }
-
-            /*
-            if(k > 0 && k < numberOfRows-1 && i > 0 && i < numberOfCollumns-1) {
-                celula.classList.add('empty');
-            } */
-
-            /*
-            if(k <= i) {
-                celula.classList.add('empty');
-            } */
-
-            /*
-            if(k + i < numberOfCollumns-1) {
-                celula.classList.add('empty');
-            } */
-
-            /*
-            if(k + i < numberOfRows-1) {
-                celula.classList.add('empty');
-            } */
+            drawRule(celula, numberOfRows, numberOfCollumns, k, i);
         }
+    }
+}
+
+function rule1(celula, numberOfRows, numberOfCollumns, k, i) {
+    if (k == 0 || i == 0 || k == numberOfRows - 1 || i == numberOfCollumns - 1) {
+        celula.classList.add('empty');
+    }
+}
+
+function rule2(celula, numberOfRows, numberOfCollumns, k, i) {
+    if (k > 0 && i > 0 && k < numberOfRows - 1 && i < numberOfCollumns - 1) {
+        celula.classList.add('empty');
+    }
+}
+
+function rule3(celula, numberOfRows, numberOfCollumns, k, i) {
+    if( k == i ) { 
+        celula.classList.add('empty');
+    }
+}
+
+function rule4(celula, numberOfRows, numberOfCollumns, k, i) {
+    if( k + i == numberOfRows - 1) { 
+        celula.classList.add('empty');
     }
 }
