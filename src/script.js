acdc8ec7123187f4ef8f3f45998b3650e279b1e5
Prof. Hugo - SENAC <hugo.tannus@go.senac.br>
2024-09-20 15:11:44 -0300

    Verificação prévia para movimentação do jogador.

--- src/script.js
+++ src/script.js
@@ -1,32 +1,42 @@
+window.addEventListener("keydown", function(event) {
+    const next = nextPosition(event.code);
+
+    if(verifyPosition(next)) movePlayer(next);
+})
+
 function Player(posX, posY){
     this.x = posX;
     this.y = posY;
 }
 
 const player = new Player(0, 0);
-
+const celulas = document.querySelectorAll('.cell');
 const playerElement = document.querySelector('.player');
 
-playerElement.addEventListener("click", function(){
-    window.alert("Clicou no jogador");
-})
+function nextPosition(keycode) {
+    let { x, y } = player;
+    
+    if(keycode == "ArrowUp") x--;
+    if(keycode == "ArrowDown") x++;
+    if(keycode == "ArrowLeft") y--;
+    if(keycode == "ArrowRight") y++;
 
-window.addEventListener("keydown", function(event) {
-    nextPosition(event.code);
-})
+    return {x, y};
+}
 
-const celulas = document.querySelectorAll('.cell');
-console.log(celulas);
+function movePlayer(position){
+    let {x, y} = position;
 
-function nextPosition(keycode) {  
-    if(keycode == "ArrowUp") player.x--;
-    if(keycode == "ArrowDown") player.x++;
-    if(keycode == "ArrowLeft") player.y--;
-    if(keycode == "ArrowRight") player.y++;
-
-    console.log(keycode, player);
+    player.x = x;
+    player.y = y;
 
     const K = player.x * 4 + player.y;
     
     celulas[K].append(playerElement);
+}
+
+function verifyPosition(position){
+    let {x, y} = position;
+
+    return x >= 0 && x < 4 && y >= 0 && y < 4;
 }
