ab1dfa7ef554450ed66facd787ec71a6e51ecf95
Prof. Hugo - SENAC <hugo.tannus@go.senac.br>
2024-09-24 16:13:58 -0300

    Função para criar mapa.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+buildGameBoard(8, 8, regra0);
+
 const player = new Player(0, 0);
 const playerElement = document.querySelector('.player');
 
@@ -48,4 +50,31 @@ function verifyPosition(position) {
 
 function calculaPosicao(qtd) {
     return `${qtd * DIST_SALTO + MARGIN_FIX}px`;
+}
+
+function buildGameBoard(numRows, numCols, _regra) {
+    const game = document.getElementById("game");
+    const board = document.createElement('div');
+    board.classList.add('board');
+    
+    for (let k = 0; k < numRows; k++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        board.append(row);
+        
+        for (let i = 0; i < numCols; i++) {
+            const celula = document.createElement('div');
+            celula.classList.add('cell');
+            row.append(celula);
+
+            const devePreencher = regra(numRows, numCols, k, i);
+            
+            if(devePreencher) celula.classList.add('flag');
+        }
+    }
+    game.append(board);
+}
+
+function regra0() {
+    return false;
 }
