d62cec08dd3e7b1a6e3c1489e4c048a53f2cfc1d
Prof. Hugo - SENAC <hugo.tannus@go.senac.br>
2024-09-25 16:41:26 -0300

    Cria quantidade de células de acordo com mapa de caracteres.

--- lab/importingMap/script.js
+++ lab/importingMap/script.js
@@ -1,58 +1,18 @@
-const DIST_SALTO = 66;
-const MARGIN_FIX = 4;
-const NUM_ROWS = 8;
-const NUM_COLS = 6;
+const boardMap = [
+    [ "#", "#", "#", "#", "#", "#", "#", "#" ],
+    [ "#", ".", ".", ".", ".", ".", ".", "#" ],
+    [ "#", ".", ".", ".", "#", ".", ".", "#" ],
+    [ "#", ".", "#", "G", ".", ".", ".", "#" ],
+    [ "#", ".", ".", "G", "B", "#", ".", "#" ],
+    [ "#", ".", ".", "#", ".", "B", ".", "#" ],
+    [ "#", ".", ".", "P", ".", ".", ".", "#" ],
+    [ "#", "#", "#", "#", "#", "#", "#", "#" ]
+];
+
+const NUM_ROWS = boardMap.length;
+const NUM_COLS = boardMap[0].length;
 
 buildGameBoard(NUM_ROWS, NUM_COLS);
-
-const player = new Player(0, 0);
-const playerElement = document.querySelector('.player');
-
-playerElement.style.top = calculaPosicao(0);
-playerElement.style.left = calculaPosicao(0);
-
-window.addEventListener("keydown", function (event) {
-    event.preventDefault();
-    const next = player.nextPosition(event.code);
-
-    if (verifyPosition(next)) {
-        player.moveTo(next, playerElement);
-    }
-});
-
-function Player(posX, posY) {
-    this.x = posX;
-    this.y = posY;
-
-    this.nextPosition = function (keycode) {
-        let { x, y } = this;
-
-        if (keycode == "ArrowUp") x--;
-        if (keycode == "ArrowDown") x++;
-        if (keycode == "ArrowLeft") y--;
-        if (keycode == "ArrowRight") y++;
-
-        return { x, y };
-    }
-
-    this.moveTo = function (position, element) {
-        this.x = position.x;
-        this.y = position.y;
-
-        element.style.top = calculaPosicao(this.x);
-        element.style.left = calculaPosicao(this.y);
-    }
-}
-
-function verifyPosition(position) {
-    let { x, y } = position;
-
-    return x >= 0 && x < NUM_ROWS && y >= 0 && y < NUM_COLS;
-}
-
-function calculaPosicao(qtd) {
-    return `${qtd * DIST_SALTO + MARGIN_FIX}px`;
-}
 
 function createGameElement(elementName, className, parentNode) {
     const element = document.createElement(elementName);
@@ -73,6 +33,4 @@ function buildGameBoard(numRows, numCols) {
             createGameElement('div', 'cell', row);
         }
     }
-
-    createGameElement('div', 'player', board);
 }
