e65f92beeeffa53ad28e4c6b507ad60a42c66a27
Hugo Tannus <hctannus@gmail.com>
2024-09-27 05:32:57 -0300

    Cria função `createBoardPiece`

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
 const boardMap = [
-    [ "#", "#", "#", "#", "#", "#", "#", "#" ],
-    [ "#", ".", ".", ".", ".", ".", ".", "#" ],
-    [ "#", ".", ".", ".", "#", ".", ".", "#" ],
-    [ "#", ".", "#", "G", ".", ".", ".", "#" ],
-    [ "#", ".", ".", "G", "B", "#", ".", "#" ],
-    [ "#", ".", ".", "#", ".", "B", ".", "#" ],
-    [ "#", ".", ".", "P", ".", ".", ".", "#" ],
-    [ "#", "#", "#", "#", "#", "#", "#", "#" ]
+    ["#", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", ".", ".", ".", ".", ".", ".", "#"],
+    ["#", ".", ".", ".", "#", ".", ".", "#"],
+    ["#", ".", "#", "G", ".", ".", ".", "#"],
+    ["#", ".", ".", "G", "B", "#", ".", "#"],
+    ["#", ".", ".", "#", ".", "B", ".", "#"],
+    ["#", ".", ".", "P", ".", ".", ".", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "#"]
 ];
 
 const NUM_ROWS = boardMap.length;
@@ -15,12 +15,18 @@ const NUM_COLS = boardMap[0].length;
 const DIST_SALTO = 66;
 const MARGIN_FIX = 4;
 
-const pieces = buildGameBoard(NUM_ROWS, NUM_COLS);
-
-const player = new Piece(pieces.player.x, pieces.player.y);
+const piecesPosition = buildGameBoard(NUM_ROWS, NUM_COLS);
 const board = document.querySelector('.board');
 
-player.insertElementInto(board);
+const player = createBoardPiece(piecesPosition.player, 'player');
+const boxes = piecesPosition.boxes.map(box => createBoardPiece(box));
+
+function createBoardPiece(position, className='box') {
+    const piece = new Piece(position.x, position.y);
+    piece.insertElementInto(board, className);
+
+    return piece
+}
 
 window.addEventListener("keydown", function (event) {
     event.preventDefault();
@@ -52,12 +58,12 @@ function Piece(posX, posY) {
         this.updatePiecePosition();
     }
 
-    this.insertElementInto = function(board){
-        this.element = createGameElement('div', ['piece', 'player'], board);
+    this.insertElementInto = function (parentElement, className) {
+        this.element = createGameElement('div', ['piece', className], parentElement);
         this.updatePiecePosition();
     }
-    
-    this.updatePiecePosition = function() {
+
+    this.updatePiecePosition = function () {
         this.element.style.top = calculaPosicao(this.x);
         this.element.style.left = calculaPosicao(this.y);
     }
@@ -82,30 +88,28 @@ function createGameElement(elementName, classList, parentNode) {
 }
 
 function buildGameBoard(numRows, numCols) {
+    const piecesPosition = { 
+        player: null,
+        boxes: []
+    }
+
     const game = document.getElementById("game");
     const board = createGameElement('div', ['board'], game);
 
-    const piecesPosition = {}
-
     for (let i = 0; i < numRows; i++) {
-        const row = createGameElement('div',['row'], board);
+        const row = createGameElement('div', ['row'], board);
 
         for (let j = 0; j < numCols; j++) {
             const cell = createGameElement('div', ['cell'], row);
 
             const char = boardMap[i][j];
-            
-            if(char === '#')cell.classList.add(['wall']);
-            if(char === 'G')cell.classList.add(['goal']);
-            if(char === 'P')piecesPosition.player = {x:i, y:j};
-            if(char === 'B'){
-                const box = createGameElement('div', ['piece', 'box'], board);
-                box.style.top = calculaPosicao(i);
-                box.style.left = calculaPosicao(j);
-            };
+
+            if (char === '#') cell.classList.add(['wall']);
+            if (char === 'G') cell.classList.add(['goal']);
+            if (char === 'P') piecesPosition.player = { x: i, y: j };
+            if (char === 'B') piecesPosition.boxes.push({ x: i, y: j });
         }
     }
-
 
     return piecesPosition;
 }
