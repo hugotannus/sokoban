5ec89f0f47ad766057d80f130c73a7d3f18b711d
Hugo Tannus <hctannus@gmail.com>
2024-09-27 05:40:59 -0300

    Simplifica estilos e função de criação de elementos.

--- src/script.js
+++ src/script.js
@@ -59,7 +59,7 @@ function Piece(posX, posY) {
     }
 
     this.insertElementInto = function (parentElement, className) {
-        this.element = createGameElement('div', ['piece', className], parentElement);
+        this.element = createGameElement(className, parentElement);
         this.updatePiecePosition();
     }
 
@@ -79,9 +79,9 @@ function calculaPosicao(qtd) {
     return `${qtd * DIST_SALTO + MARGIN_FIX}px`;
 }
 
-function createGameElement(elementName, classList, parentNode) {
+function createGameElement(classList, parentNode, elementName='div') {
     const element = document.createElement(elementName);
-    element.classList.add(...classList);
+    element.classList.add(classList);
     parentNode.append(element);
 
     return element;
@@ -94,18 +94,18 @@ function buildGameBoard(numRows, numCols) {
     }
 
     const game = document.getElementById("game");
-    const board = createGameElement('div', ['board'], game);
+    const board = createGameElement('board', game);
 
     for (let i = 0; i < numRows; i++) {
-        const row = createGameElement('div', ['row'], board);
+        const row = createGameElement('row', board);
 
         for (let j = 0; j < numCols; j++) {
-            const cell = createGameElement('div', ['cell'], row);
+            const cell = createGameElement('cell', row);
 
             const char = boardMap[i][j];
 
-            if (char === '#') cell.classList.add(['wall']);
-            if (char === 'G') cell.classList.add(['goal']);
+            if (char === '#') cell.classList.add('wall');
+            if (char === 'G') cell.classList.add('goal');
             if (char === 'P') piecesPosition.player = { x: i, y: j };
             if (char === 'B') piecesPosition.boxes.push({ x: i, y: j });
         }
