342466edecbda5be9e3401300a7040738ccba9f1
Prof. Hugo - SENAC <hugo.tannus@go.senac.br>
2024-09-27 15:10:27 -0300

    Transfere construtor `Piece` para novo arquivo `piece.js`

--- src/script.js
+++ src/script.js
@@ -19,30 +19,6 @@ window.addEventListener("keydown", function (event) {
     }
 });
 
-function Piece(posX, posY) {
-    this.x = posX;
-    this.y = posY;
-
-    this.nextPosition = function (keycode) {
-        let { x, y } = this;
-
-        if (keycode == "ArrowUp") x--;
-        if (keycode == "ArrowDown") x++;
-        if (keycode == "ArrowLeft") y--;
-        if (keycode == "ArrowRight") y++;
-
-        return { x, y };
-    }
-
-    this.moveTo = function (position, element) {
-        this.x = position.x;
-        this.y = position.y;
-
-        element.style.top = calculaPosicao(this.x);
-        element.style.left = calculaPosicao(this.y);
-    }
-}
-
 function verifyPosition(position) {
     let { x, y } = position;
 
