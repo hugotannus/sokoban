b2c7d4d4f7706b7de91adf60dc7be1e3d5f56d88
Hugo Tannus <hctannus@gmail.com>
2024-09-29 09:22:33 -0300

    Adiciona contador de movimentos

--- src/script.js
+++ src/script.js
@@ -3,34 +3,48 @@ const board = document.querySelector('.board');
 
 const player = createBoardPiece(pieces.player, 'player');
 const boxes = pieces.boxes.map(box => createBoardPiece(box));
+let moves = 0;
 
 window.addEventListener("keydown", function (event) {
     event.preventDefault();
-    handlePieceMovement(event.code);
+    
+    const VALID_KEYS = [ "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", ];
+
+    if(VALID_KEYS.includes(event.code)) {
+        handlePieceMovement(event.code);
+    }
 });
+
+function updateMovesCounter() {
+    const movesElement = document.getElementById('moves');
+    movesElement.innerText = ++moves;
+}
 
 function createBoardPiece(position, className = 'box') {
     const piece = new Piece(position.x, position.y);
     piece.insertElementInto(board, className);
-
+    
     return piece
 }
 
 function handlePieceMovement(keycode) {
     const nextPosition = player.nextPosition(keycode);
     const foundBox = boxes.find(box => box.x == nextPosition.x && box.y == nextPosition.y);
-
+    
     if (foundBox) {
         const nextBoxPosition = foundBox.nextPosition(keycode);
-
+        
         if (verifyPosition(nextBoxPosition)) {
             foundBox.moveTo(nextBoxPosition);
             player.moveTo(nextPosition);
 
+            updateMovesCounter();
+            
             setTimeout(verifyVictory, 500);
         }
     } else if (verifyPosition(nextPosition)) {
         player.moveTo(nextPosition);
+        updateMovesCounter();
     }
 }
 
