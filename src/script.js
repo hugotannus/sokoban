65c9c63235af542799aecb07b1b12bd2a5ad062f
Prof. Hugo - SENAC <hugo.tannus@go.senac.br>
2024-10-01 14:14:30 -0300

    Exercício: implementação de movimentação das caixas

--- src/script.js
+++ src/script.js
@@ -14,12 +14,40 @@ for (let box of pieces.boxes) {
 
 window.addEventListener("keydown", function (event) {
     event.preventDefault();
-    const next = player.nextPosition(event.code);
 
-    if (verifyPosition(next)) {
-        player.moveTo(next);
-    }
+    handlePieceMovement(event.code);
 });
+
+/** Tarefa #1: implementar função para localizar uma caixa à partir de um
+ * uma dada coordenada.
+*/
+function findBoxAtPosition(position) {
+    // modificar linha(s) de código abaixo
+    return null;
+}
+
+/** Tarefa #2: modificar a função abaixo de forma a tratar tando a movimentação
+ * do jogador quanto das caixas.
+*/
+function handlePieceMovement(keycode){
+    // Variável destinada ao pré-cálculo da posição do jogador
+    const next = player.nextPosition(keycode);
+    // (Modificar) Variável para detectar a "presença" de outra peça
+    const foundPice = null;
+
+    // Implementar lógica caso encontre uma outra peça no caminho.
+    if(foundPice) {
+
+    }
+    // E caso não encontre outra peça...
+    else {
+        // Faça as modificações que forem necessárias para manter o
+        // funcionamento do jogo.
+        if (verifyPosition(next)) {
+            player.moveTo(next);
+        }
+    }
+}
 
 function createBoardPiece(piecePosition, className) {
     const piece = new Piece(piecePosition.x, piecePosition.y);
